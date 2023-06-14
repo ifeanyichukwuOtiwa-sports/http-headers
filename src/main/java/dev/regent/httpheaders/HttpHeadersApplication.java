44720c4bdc53fee9f65d6e4a6c225975fd96dc44
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2023-06-14 17:32:14 +0100

    initial commit

---
+++ src/main/java/dev/regent/httpheaders/HttpHeadersApplication.java
@@ -0,0 +1,13 @@
+package dev.regent.httpheaders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpHeadersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpHeadersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/regent/httpheaders/HttpHeadersApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.regent.httpheaders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpHeadersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
